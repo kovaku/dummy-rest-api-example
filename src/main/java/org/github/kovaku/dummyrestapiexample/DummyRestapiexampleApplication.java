3016ee2b5356be108c0a80720b5071d364d3865f
Zsolt Kovacs <v-zskovacs@hotels.com>
2019-10-11 19:08:50 +0200

    Initial commit

---
+++ src/main/java/org/github/kovaku/dummyrestapiexample/DummyRestapiexampleApplication.java
@@ -0,0 +1,13 @@
+package org.github.kovaku.dummyrestapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyRestapiexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyRestapiexampleApplication.class, args);
+	}
+
+}
